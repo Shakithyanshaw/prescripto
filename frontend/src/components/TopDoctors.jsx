672f7762b99aa4bd0777f6a1191302836769dc91
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-03-28 00:48:13 +0530

    code top doctors component structure

---
+++ frontend/src/components/TopDoctors.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { doctors } from '../assets/assets';
+
+const TopDoctors = () => {
+  return (
+    <div>
+      <h1>Top Doctors Book</h1>
+      <p>Simply browse through our extensive list of trusted doctors</p>
+      <div>
+        {doctors.slice(0, 10).map((item, index) => (
+          <div>
+            <img src={item.image} alt="" />
+            <div>
+              <div>
+                <p></p>
+                <p>Avaiable</p>
+              </div>
+              <p>{item.name}</p>
+              <p>{item.speciality}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <button>More</button>
+    </div>
+  );
+};
+
+export default TopDoctors;
