3b01e70144485813b19d2ad856c2541ac3ebacd5
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-03-29 00:36:05 +0530

    add css properties for banner component

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import { assets } from '../assets/assets';
+import { useNavigate } from 'react-router-dom';
 
 const Banner = () => {
+  const navigate = useNavigate();
   return (
-    <div>
+    <div className="flex bg-primary rounded-lg px-6 sm:px-10 md:px-12 my-20 md:mx-10">
       {/*--------------Left Side--------------*/}
-      <div>
-        <div>
+      <div className="flex-1 py-8 sm:py-10 md:py-16 lg:py-24 lg:pl-5">
+        <div className="text-xl sm:text-2xl md:text-3xl lg:text-5xl font-semibold text-white">
           <p>Book Appointment With </p>
-          <p>100+ Trusted Doctors</p>
+          <p className="mt-4">100+ Trusted Doctors</p>
         </div>
-        <button>Create Account</button>
+        <button
+          onClick={() => {
+            navigate('/login');
+            scrollTo(0, 0);
+          }}
+          className="bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all cursor-pointer"
+        >
+          Create Account
+        </button>
       </div>
       {/*--------------Right Side--------------*/}
-      <div>
-        <img src={assets.appointment_img} alt="" />
+      <div className="hidden md:block md:w-1/2 lg:w-92.5 relative">
+        <img
+          className="w-full absolute bottom-0 right-0 max-w-md"
+          src={assets.appointment_img}
+          alt=""
+        />
       </div>
     </div>
   );
