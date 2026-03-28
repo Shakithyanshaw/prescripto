8d082d8d27d6764973ac49a8f2ccc98f11968034
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-03-29 00:54:59 +0530

    add css properties to footer components

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,12 +3,12 @@ import { assets } from '../assets/assets';
 
 const Footer = () => {
   return (
-    <div>
-      <div>
+    <div className="md:mx-10">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         {/*--------------Left Side--------------*/}
         <div>
-          <img src={assets.logo} alt="" />
-          <p>
+          <img className="mb-5 w-40" src={assets.logo} alt="" />
+          <p className="w-full md:w-2/3 text-gray-600 leading-6">
             With a strong foundation in trusted healthcare practices, we have
             evolved to make doctor appointments simple, fast, and accessible.
             Our platform connects patients with experienced doctors, ensuring
@@ -17,8 +17,8 @@ const Footer = () => {
         </div>
         {/*--------------Center Side--------------*/}
         <div>
-          <p>Company</p>
-          <ul>
+          <p className="text-xl font-medium mb-5">Company</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
             <li>Home</li>
             <li>About Us</li>
             <li>Contact Us</li>
@@ -27,8 +27,8 @@ const Footer = () => {
         </div>
         {/*--------------Right Side--------------*/}
         <div>
-          <p>Get in Touch</p>
-          <ul>
+          <p className="text-xl font-medium mb-5">Get in Touch</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
             <li>+94-77-111-2224</li>
             <li>prescripto@gmail.com</li>
           </ul>
@@ -37,7 +37,7 @@ const Footer = () => {
       {/*--------------Copy Right--------------*/}
       <div>
         <hr />
-        <p>
+        <p className="py-5 text-sm text-center">
           © 2026 Prescripto. All rights reserved. Your trusted partner in
           healthcare.
         </p>
