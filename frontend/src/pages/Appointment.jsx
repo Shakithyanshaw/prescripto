bd13ffb147a92b46e46a918b839a2652140f2286
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-03-30 00:57:04 +0530

    implement responsive doctor details layout with styling

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -24,29 +24,41 @@ const Appointment = () => {
     docInfo && (
       <div>
         {/*--------------Doctors Details--------------*/}
-        <div>
+        <div className="flex flex-col sm:flex-row gap-4">
           <div>
-            <img src={docInfo.image} alt="" />
+            <img
+              className="bg-primary w-full sm:max-w-72 rounded-lg"
+              src={docInfo.image}
+              alt=""
+            />
           </div>
-          <div>
+          <div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 -mt-20 sm:mt-0">
             {/*--------------Doctors Info--------------*/}
-            <p>
-              {docInfo.name} <img src={assets.verified_icon} alt="" />
+            <p className="flex items-center gap-2 text-2xl font-medium text-gray-900">
+              {docInfo.name}{' '}
+              <img className="w-5" src={assets.verified_icon} alt="" />
             </p>
-            <div>
+            <div className="flex items-center gap-2 text-sm mt-1 text-gray-600">
               <p>
                 {docInfo.degree} - {docInfo.speciality}
               </p>
-              <button>{docInfo.experience}</button>
+              <button className="py-0.5 px-2 border text-sm rounded-full">
+                {docInfo.experience}
+              </button>
             </div>
 
             {/*--------------Doctors About--------------*/}
             <div>
-              <p>
+              <p className="flex items-center gap-1 text-sm font-medium text-gray-900 mt-3">
                 About <img src={assets.info_icon} alt="" />
               </p>
-              <p>{docInfo.about}</p>
+              <p className="text-sm text-gray-500 max-w-175 mt-1">
+                {docInfo.about}
+              </p>
             </div>
+            <p>
+              Appointment fee: <span>{docInfo.fees}</span>{' '}
+            </p>
           </div>
         </div>
       </div>
