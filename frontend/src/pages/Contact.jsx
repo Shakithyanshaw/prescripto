034f5b3d3a35f2478309cbee60a628ffdbb97bf1
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-03-31 01:24:48 +0530

    design Contact page with responsive layout and interactive button

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { assets } from '../assets/assets';
 
 const Contact = () => {
   return (
     <div>
-      <h1>Contact</h1>
+      <div className="text-center text-2xl pt-10 text-gray-500">
+        <p>
+          CONTACT <span className="text-gray-700 font-medium">US</span>
+        </p>
+      </div>
+
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm">
+        <img className="w-full max-w-90" src={assets.contact_image} alt="" />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-lg text-gray-600">Our OFFICE</p>
+          <p className="text-gray-500">
+            54709 Willms Station <br />
+            Suite 350, Washington, USA
+          </p>
+          <p className="text-gray-500">
+            Tel: +94-77-111-2224 <br />
+            Email: prescripto@gmail.com
+          </p>
+          <p className="font-semibold text-lg text-gray-600">
+            Careers at PRESCRIPTO
+          </p>
+          <p className="text-gray-500">
+            Learn more about our teams and job openings.
+          </p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500 cursor-pointer">
+            Explore Jobs
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
