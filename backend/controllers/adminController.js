eaed1f1d70035cd2dc065b4e7c3de620839be08d
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-04-18 11:51:35 +0530

    complete addDoctor API

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,5 +1,7 @@
 import validator from 'validator';
 import bcrypt from 'bcrypt';
+import { v2 as cloudinary } from 'cloudinary';
+import doctorModel from '../models/doctorModel.js';
 
 //API for adding doctors
 const addDoctor = async (req, res) => {
@@ -50,7 +52,41 @@ const addDoctor = async (req, res) => {
     //Hashing doctor password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-  } catch (error) {}
+
+    //Upload image to cloudinary
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: 'image',
+    });
+    const imageUrl = imageUpload.secure_url;
+
+    const doctorData = {
+      name,
+      email,
+      image: imageUrl,
+      password: hashedPassword,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address: JSON.parse(address),
+      date: Date.now(),
+    };
+
+    const newDoctor = new doctorModel(doctorData);
+    await newDoctor.save();
+
+    res.json({
+      success: true,
+      message: 'Doctor Added Successfully',
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 export { addDoctor };
