08395269c296bd839dc78c8ca19e007e888f13d7
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-04-23 00:29:46 +0530

    add css properties to admin login page

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -4,20 +4,30 @@ const Login = () => {
   const [state, setState] = useState('Admin');
 
   return (
-    <form>
-      <div>
-        <p>
-          <span>{state}</span> Login
+    <form className="min-h-[80vh] flex items-center">
+      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-85 sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg">
+        <p className="text-2xl font-semibold m-auto">
+          <span className="text-primary">{state}</span> Login
         </p>
-        <div>
+        <div className="w-full">
           <p>Email</p>
-          <input type="email" required />
+          <input
+            className="border border-[#DADADA] rounded w-full p-2 mt-1"
+            type="email"
+            required
+          />
         </div>
-        <div>
+        <div className="w-full">
           <p>Password</p>
-          <input type="password" required />
+          <input
+            className="border border-[#DADADA] rounded w-full p-2 mt-1"
+            type="password"
+            required
+          />
         </div>
-        <button>Login</button>
+        <button className="bg-primary text-white w-full py-2 rounded-md text-base">
+          Login
+        </button>
       </div>
     </form>
   );
