3f2a02232b0c1f77a703fab232ce2968656fd177
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-05-03 12:03:10 +0530

    integrate get doctors data API

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,15 +1,36 @@
-import { createContext } from 'react';
-import { doctors } from '../assets/assets';
+import { createContext, use, useEffect, useState } from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const currencySymbol = '$';
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const [doctors, setDoctors] = useState([]);
 
   const value = {
     doctors,
     currencySymbol,
   };
+
+  const getDoctorsdata = async () => {
+    try {
+      const { data } = await axios.post(backendUrl + '/api/doctor/list');
+      if (data.success) {
+        setDoctors(data.doctors);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getDoctorsdata();
+  }, []);
 
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
