028ae2d0746be72d8cac84e726f8e264a9accdf7
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-05-05 14:53:22 +0530

    Add conditional image edit preview with upload option

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,18 +1,47 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+import { assets } from '../assets/assets';
 
 const MyProfile = () => {
-  const { userData, setUserData } = useContext(AppContext);
+  const { userData, setUserData, token, backendUrl, loadUserProfileData } =
+    useContext(AppContext);
   const [isEdit, setIsEdit] = useState(false);
+  const [image, setImage] = useState(false);
+
+  const updateUserProfileData = async () => {};
 
   return (
     userData && (
       <div className="max-w-lg flex flex-col gap-2 text-sm">
-        <img
-          className="w-36 rounded"
-          src={userData.image}
-          alt={userData.name}
-        />
+        {isEdit ? (
+          <label htmlFor="image">
+            <div className="inline-block relative cursor-pointer">
+              <img
+                className="w-36 rounded opacity-75"
+                src={image ? URL.createObjectURL(image) : userData.image}
+                alt=""
+              />
+              <img
+                className="w-10 absolute bottom-12 right-12"
+                src={image ? '' : assets.upload_icon}
+                alt=""
+              />
+            </div>
+            <input
+              onChange={(e) => setImage(e.target.files[0])}
+              type="file"
+              id="image"
+              hidden
+            />
+          </label>
+        ) : (
+          <img
+            className="w-36 rounded"
+            src={userData.image}
+            alt={userData.name}
+          />
+        )}
+
         {isEdit ? (
           <input
             className="bg-gray-50 text-3xl font-medium max-w-60 mt-4"
