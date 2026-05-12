8c949b234938df513e630c8f6c9f705ff04c472f
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-05-12 12:45:51 +0530

    implement Razorpay payment integration for appointment transactions

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -66,6 +66,25 @@ const MyAppointments = () => {
     }
   };
 
+  const initPay = (order) => {};
+
+  const appointmentRazorpay = async (appointmentId) => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + '/api/user/payment-razorpay',
+        { appointmentId },
+        { headers: { token } },
+      );
+
+      if (data.success) {
+        initPay(data.order);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
   useEffect(() => {
     if (token) {
       getUserAppointments();
@@ -109,7 +128,10 @@ const MyAppointments = () => {
             <div></div>
             <div className="flex flex-col gap-2 justify-end">
               {!item.cancelled && (
-                <button className="tex-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded cursor-pointer hover:bg-primary hover:text-white transition-all duration-300">
+                <button
+                  onClick={() => appointmentRazorpay(item._id)}
+                  className="tex-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded cursor-pointer hover:bg-primary hover:text-white transition-all duration-300"
+                >
                   Pay Online
                 </button>
               )}
