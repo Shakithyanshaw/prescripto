f389c86606ea3fc4eb498c07ca5a50af2c66dbb2
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-05-18 01:21:20 +0530

    create API to mark appointment completed for doctor panel

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -63,4 +63,25 @@ const appointmentsDoctor = async (req, res) => {
   }
 };
 
+// API to mark appointment completed for doctor panel
+const appointmentComplete = async (req, res) => {
+  try {
+    const { docId, appointmentId } = req.body;
+
+    const appointmentData = await appointmentModel.findById(appointmentId);
+
+    if (appointmentData && appointmentData.docId === docId) {
+      await appointmentModel.findByIdAndUpdate(appointmentId, {
+        isCompleted: true,
+      });
+      return res.json({ success: true, message: 'Appointment Completed' });
+    } else {
+      return res.json({ success: false, message: 'Mark Failed' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
 export { changeAvailablity, doctorList, loginDoctor, appointmentsDoctor };
