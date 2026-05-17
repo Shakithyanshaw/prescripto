097760dd9ff381e7d37570c491aa884b559e079c
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-05-18 00:35:10 +0530

    add appointment management to DoctorContext with API integration

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { createContext } from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 export const DoctorContext = createContext();
 
@@ -10,10 +12,32 @@ const DoctorContextProvider = (props) => {
     localStorage.getItem('dToken') ? localStorage.getItem('dToken') : '',
   );
 
+  const [appointments, setAppointments] = useState([]);
+
+  const getApppointments = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + '/api/doctor/profile', {
+        headers: { dToken },
+      });
+      if (data.success) {
+        setAppointments(data.appointments.reverse());
+        console.log(data.appointments.reverse());
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
   const value = {
     dToken,
     setDToken,
     backendUrl,
+    appointments,
+    setAppointments,
+    getApppointments,
   };
 
   return (
